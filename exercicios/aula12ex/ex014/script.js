06026298b7ede7317a4e9efa86f336ee76e3a245
GabrielBaiaDias <102996530+GabrielBaiaDias@users.noreply.github.com>
2022-04-21 12:58:56 -0300

    main

--- exercicios/aula12ex/ex014/script.js
+++ exercicios/aula12ex/ex014/script.js
@@ -2,17 +2,17 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 13
+    var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if(hora >= 0 && hora < 12){
         //BOM DIA!
-        img.src='foto-manha.png'
+        img.src='imagens/foto-manha.png'
+        document.body.style.background = '#'e2cd9f'
     }else if(hora >= 12 && hora <= 18){
         //BOA TARDE!
-        img.src='foto-tarde.png'
+        img.src='imagens/foto-tarde.png'
     }else {
         //BOA NOITE!
-        img.src='foto-noite.png'
+        img.src='imagens/foto-noite.png'
     }       
 }
